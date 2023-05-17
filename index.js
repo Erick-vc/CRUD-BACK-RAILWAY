7f7827bd878aeaa46ca808c8c4eaf208a2253632
Erick Villalobos <villalobose143@gmail.com>
2023-05-17 12:32:00 -0500

    edit src

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 import { PORT } from './config.js';
+import { fileURLToPath } from "url";
+import {dirname, join}  from 'path';
 
-import taskRoutes from './routes/tasks.routes.js'
+
+
+import taskRoutes from './src/routes/tasks.routes.js'
 
 // import { pool } from './db.js'
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 app.use(cors());
--- src/controllers/tasks.controllers.js
+++ src/controllers/tasks.controllers.js
@@ -1,4 +1,4 @@
-import { pool } from "../db.js";
+import { pool } from "../../db.js";
 
 // ? Mostrar todas las tareas
 export const getCustomers = async (req, res) => {
